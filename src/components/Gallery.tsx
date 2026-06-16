2330fbea46b24c714eaee17e601fc85013f9b3c5
Israel Ekpenyong <israeljaxson@gmail.com>
2026-06-17 01:49:31 +0300

    initial commit

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -238,6 +238,7 @@ export default function Gallery() {
             </a>
           </div>
 
+
           {/* EDITORIAL MASONRY GRID CONSTRUCT */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
             {posts.map((post) => {
